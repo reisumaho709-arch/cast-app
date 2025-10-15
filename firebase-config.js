7cb98d9a9b5811798416c5c3b3701363b55b589c
reisumaho709-arch <reisumaho709@gmail.com>
2025-10-15 17:31:10 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHEgghO0D11XDLhg_trJlbOncZO7P9HfA",
+  authDomain: "cast-app-7ddf6.firebaseapp.com",
+  projectId: "cast-app-7ddf6",
+  storageBucket: "cast-app-7ddf6.firebasestorage.app",
+  messagingSenderId: "461458023553",
+  appId: "1:461458023553:web:50b4eadc30605fb06ed980"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
